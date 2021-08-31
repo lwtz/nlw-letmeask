e5cd64e2dd383b401bbac92147da3d023bdbdbaa
lwtz <w.h@outlook.com.br>
2021-08-31 16:12:24 -0300

    Login and Create questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { BrowserRouter, Route } from "react-router-dom"
-import { Home }                 from "./pages/Home"
-import { NewRoom }              from "./pages/NewRoom"
-import { AuthContextProvider } from "./contexts/AuthContext"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { Home }                         from "./pages/Home"
+import { Room }                         from "./pages/Room"
+import { NewRoom }                      from "./pages/NewRoom"
+import { AuthContextProvider }          from "./contexts/AuthContext"
 
 function App() {
     return (
         <BrowserRouter>
             <AuthContextProvider>
-                <Route exact path={ "/" } component={ Home } />
-                <Route path={ "/rooms/new" } component={ NewRoom } />
+                <Switch>
+                    <Route path={ "/" } exact component={ Home } />
+                    <Route path={ "/rooms/new" } exact component={ NewRoom } />
+                    <Route path={ "/rooms/:id" } component={ Room } />
+                </Switch>
             </AuthContextProvider>
         </BrowserRouter>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,42 @@
-import { useHistory }  from "react-router-dom"
-import illustrationIMG from "../assets/illustration.svg"
-import logoIMG         from "../assets/logo.svg"
-import ggIconIMG       from "../assets/google-icon.svg"
-import { Button }      from "../components/Button"
+import { FormEvent, useState } from "react"
+import { useHistory }          from "react-router-dom"
+import illustrationIMG         from "../assets/illustration.svg"
+import logoIMG                 from "../assets/logo.svg"
+import ggIconIMG               from "../assets/google-icon.svg"
+import { Button }              from "../components/Button"
 import "../styles/auth.scss"
-import { useAuth }     from "../hooks/useAuth"
+import { useAuth }             from "../hooks/useAuth"
+import { database }            from "../services/firebase"
 
 
 export function Home() {
     const history = useHistory()
     const { signInWithGoogle, user } = useAuth()
+    const [ roomCode, setRoomCode ] = useState( "" )
 
     async function handleCreateRoom() {
         if ( !user ) {
             await signInWithGoogle()
         }
         history.push( "/rooms/new" )
+    }
+
+    async function handleJoinRoom( event: FormEvent ) {
+        event.preventDefault()
+
+        if ( roomCode.trim() === "" ) {
+            return
+        }
+
+        const roomRef = await database.ref( `/rooms/${ roomCode }` ).get()
+
+        if ( !roomRef.exists() ) {
+            alert( "Room does not exist" )
+            return
+        }
+
+        history.push( `/rooms/${ roomCode }` )
+
     }
 
 
@@ -41,9 +62,11 @@ export function Home() {
                     <div className="separator">
                         ou entre em um sala
                     </div>
-                    <form action="">
+                    <form onSubmit={ handleJoinRoom }>
                         <input type="text"
                                placeholder="Digite o código da sala"
+                               onChange={ event => setRoomCode( event.target.value ) }
+                               value={ roomCode }
                         />
                         <Button type="submit">
                             Entrar na sala
